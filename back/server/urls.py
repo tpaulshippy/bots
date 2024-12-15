160c1b236826a3dd5fed0efd7debb135514514cb
Paul Shippy <paul.shippy@strongmind.com>
2024-12-15 16:45:06 -0700

    Rename django project to server and make bots app

--- back/server/urls.py
+++ back/server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bots project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
