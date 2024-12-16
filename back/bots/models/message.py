f9aca8109c639be2a1ef6993a51dac9760aa9bfd
Paul Shippy <paul.shippy@strongmind.com>
2024-12-15 17:28:47 -0700

    Add role to message

--- back/bots/models/message.py
+++ back/bots/models/message.py
@@ -6,6 +6,7 @@ class Message(models.Model):
     chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
     message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     text = models.TextField()
+    role = models.CharField(max_length=50, default='user')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
