51698649c3444b2ba46d67be25e70f001dd2302b
Paul Shippy <paul.shippy@strongmind.com>
2024-12-17 13:16:53 -0700

    Add system message

--- back/bots/views.py
+++ back/bots/views.py
@@ -8,8 +8,11 @@ def get_response_api(request, chat_id):
 
     if chat_id == 'new':
         chat = Chat.objects.create(title=user_input)
+        chat.messages.create(text=chat.get_system_message(), role='system', order=0)
+
     else:
         chat = Chat.objects.get(chat_id=chat_id)
+    
     chat.messages.create(text=user_input, role='user', order=chat.messages.count())
 
     response = chat.get_response()
