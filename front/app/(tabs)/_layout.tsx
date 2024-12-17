5ac9e198fb366adc5f1c322f960f40af885f69e4
Paul Shippy <paul.shippy@strongmind.com>
2024-12-17 12:49:06 -0700

    Slim down UI

--- front/app/(tabs)/_layout.tsx
+++ front/app/(tabs)/_layout.tsx
@@ -29,15 +29,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Chats',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
