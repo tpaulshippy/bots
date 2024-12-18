f346a4e86cbfda8c3fb1f707448fabe8319bc8d5
Paul Shippy <paul.shippy@strongmind.com>
2024-12-17 18:10:24 -0700

    Style the user/assistant messages

--- front/app/screens/chat.tsx
+++ front/app/screens/chat.tsx
@@ -6,16 +6,26 @@ import { sendChat } from "@/api/chats";
 import { FlatList } from "react-native";
 import { KeyboardAvoidingView, Platform } from "react-native";
 
+interface ChatMessage {
+  role: "user" | "assistant";
+  text: string;
+}
+
 export default function Chat() {
   const [chatId, setChatId] = useState<string>();
   const [input, setInput] = useState<string>("");
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
 
   const sendChatToServer = () => {
     sendChat(chatId, input).then((chatResponse) => {
       if (chatResponse) {
         setInput("");
-        setMessages([...messages, input, chatResponse.response]);
+        const newUserMessage: ChatMessage = { role: "user", text: input };
+        const newAssistantMessage: ChatMessage = {
+          role: "assistant",
+          text: chatResponse.response,
+        };
+        setMessages([...messages, newUserMessage, newAssistantMessage]);
         setChatId(chatResponse.chat_id);
       }
     });
@@ -31,7 +41,17 @@ export default function Chat() {
         <FlatList
           style={styles.list}
           data={messages}
-          renderItem={({ item }) => <ThemedText>{item}</ThemedText>}
+          renderItem={({ item }) => (
+            <ThemedText
+              style={
+                item.role == "user"
+                  ? styles.userMessage
+                  : styles.assistantMessage
+              }
+            >
+              {item.text}
+            </ThemedText>
+          )}
         />
         <ThemedTextInput
           autoFocus={true}
@@ -61,5 +81,18 @@ const styles = {
     margin: 12,
     borderWidth: 1,
     borderColor: "white",
+  },
+  userMessage: {
+    backgroundColor: "#003540",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  assistantMessage: {
+    // dark green
+    backgroundColor: "#003900",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
   },
 };
