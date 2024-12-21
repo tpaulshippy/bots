70467b592f23f2995d8a4200817efd5abf5b555c
Paul Shippy <paul.shippy@strongmind.com>
2024-12-21 14:35:50 -0700

    Add bot model

--- back/bots/models/__init__.py
+++ back/bots/models/__init__.py
@@ -1,3 +1,4 @@
 from .chat import Chat
 from .message import Message
 from .profile import Profile
+from .bot import Bot
--- back/bots/models/bot.py
+++ back/bots/models/bot.py
@@ -1,9 +1,11 @@
 from django.db import models
 import uuid
     
-class Profile(models.Model):
-    profile_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+class Bot(models.Model):
+    bot_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    system_prompt = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
