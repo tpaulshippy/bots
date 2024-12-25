1d66753705f5cbd897fef73ddb93eae6e92c8e54
Paul Shippy <paul.shippy@strongmind.com>
2024-12-24 20:47:18 -0700

    Add user account model with pin

--- back/bots/admin.py
+++ back/bots/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Chat, Message, Profile, Bot
+from .models import Chat, Message, Profile, Bot, UserAccount
 
 admin.site.register(Chat)
 admin.site.register(Message)
 admin.site.register(Profile)
 admin.site.register(Bot)
-
+admin.site.register(UserAccount)
--- back/bots/models/__init__.py
+++ back/bots/models/__init__.py
@@ -2,3 +2,4 @@ from .chat import Chat
 from .message import Message
 from .profile import Profile
 from .bot import Bot
+from .user_account import UserAccount
