8cec527566c026f8deaa79b68366521ce0e79e1a
Paul Shippy <paul.shippy@strongmind.com>
2025-01-09 21:48:01 -0700

    Update schema per feedback

--- back/bots/models/ai_model.py
+++ back/bots/models/ai_model.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
     
 class AiModel(models.Model):
     model_id = models.CharField(max_length=255, unique=True, db_index=True)
     name = models.CharField(max_length=255)
-    input_token_cost = models.FloatField(default=1.0)
-    output_token_cost = models.FloatField(default=1.0)
+    input_token_cost = models.FloatField(
+        default=1.0,
+        validators=[MinValueValidator(0.0)]
+    )
+    output_token_cost = models.FloatField(
+        default=1.0,
+        validators=[MinValueValidator(0.0)]
+    )
     is_default = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,3 +19,11 @@ class AiModel(models.Model):
 
     def __str__(self):
         return self.name
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['is_default'],
+                condition=models.Q(is_default=True),
+                name='unique_default_model'
+            )
+        ]
