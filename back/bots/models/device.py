db0a325f95bf26eebf68d906bcc1351374bc6bac
Paul Shippy <paul.shippy@strongmind.com>
2025-01-18 11:33:08 -0700

    Notify on new chats

--- back/bots/models/device.py
+++ back/bots/models/device.py
@@ -1,11 +1,28 @@
 from django.conf import settings
 from django.db import models
 import uuid
+import requests
+
+class Notification:
+    def __init__(self, to, sound, title, body, data):
+        self.to = to
+        self.sound = sound
+        self.title = title
+        self.body = body
+        self.data = data
+        
+
+class NotificationClient:
+    def notify(self, notification):
+        payload = notification.__dict__
+        requests.post('https://exp.host/--/api/v2/push/send', json=payload)
+        
     
 class Device(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        related_name='devices',
         null=True
     )
     device_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
@@ -19,3 +36,14 @@ class Device(models.Model):
 
     def __str__(self):
         return self.user.email + ' - ' + self.notification_token
+    
+    def notify(self, chat, is_new_chat):
+        if self.notify_on_new_chat and is_new_chat:
+            NotificationClient().notify(Notification(
+                to=self.notification_token,
+                sound='default',
+                title=f"{chat.profile.name} asked {chat.bot.name} a question",
+                body=chat.title,
+                data={'chat_id': str(chat.chat_id)}
+            ))
+            
--- back/bots/signals.py
+++ back/bots/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserAccount
+from .models import UserAccount, Chat
 
 @receiver(post_save, sender=User)
 def manage_user_profile(sender, instance, created, **kwargs):
@@ -9,3 +9,9 @@ def manage_user_profile(sender, instance, created, **kwargs):
         UserAccount.objects.create(user=instance)
     else:
         instance.user_account.save()
+
+@receiver(post_save, sender=Chat)
+def notify_chat(sender, instance, created, **kwargs):
+    if created:
+        for device in instance.user.devices.all():
+            device.notify(instance, True)
