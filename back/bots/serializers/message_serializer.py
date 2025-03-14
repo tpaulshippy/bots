1a9c3eb284168822912a08039392f942cdf6a6ad
Paul Shippy <paul.shippy@strongmind.com>
2025-03-14 12:32:40 -0700

    Add image url

--- back/bots/serializers/message_serializer.py
+++ back/bots/serializers/message_serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from bots.models import Message
-
+import boto3
+from django.conf import settings
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Message
         fields = [
@@ -14,4 +17,16 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
             'input_tokens',
             'output_tokens',
             'created_at', 
-            'modified_at'] 
+            'modified_at',
+            'image_url'
+        ]
+
+    def get_image_url(self, obj):
+        if obj.image_filename:
+            s3 = boto3.client('s3')
+            bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+            image_url = s3.generate_presigned_url('get_object', 
+                Params={'Bucket': bucket_name, 'Key': obj.image_filename}, 
+                ExpiresIn=3600)
+            return image_url
+        return None
