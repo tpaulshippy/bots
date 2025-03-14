4d614fa5c38eec67d1aae62adf3a2eeb4ca9ed91
Paul Shippy <paul.shippy@strongmind.com>
2025-03-13 22:21:51 -0700

    Show dates in admin

--- back/bots/admin.py
+++ back/bots/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
+from django.apps import apps
 from .models import Chat, Message, Profile, Bot, UserAccount, UsageLimitHit, AiModel, Device, RevenueCatWebhookEvent
 
-admin.site.register(Chat)
-admin.site.register(Message)
-admin.site.register(Profile)
-admin.site.register(Bot)
-admin.site.register(UserAccount)
-admin.site.register(UsageLimitHit)
-admin.site.register(AiModel)
-admin.site.register(Device)
-admin.site.register(RevenueCatWebhookEvent)
+class BaseAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        return ['created_at', 'modified_at']
+
+def register_all_models():
+    app_models = apps.get_models()
+    for model in app_models:
+        if hasattr(model, 'created_at') and hasattr(model, 'modified_at'):
+            admin_class = type(f"{model.__name__}Admin", (BaseAdmin,), {})
+            admin.site.register(model, admin_class)
+
+register_all_models()
