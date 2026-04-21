392bbd3e2c2a5bf047ba9b43be2c85861bd62b71
Paul Shippy <pshippy@gmail.com>
2026-04-21 04:30:52 +0000

    Remove unused status import in flashcard_viewset

--- back/bots/viewsets/flashcard_viewset.py
+++ back/bots/viewsets/flashcard_viewset.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.exceptions import NotFound
 from django.db.models import Count, Max
 import uuid
